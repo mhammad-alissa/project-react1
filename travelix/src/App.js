3eb0aeab9b74a2dd39baddc270d7fc49ba0049b6
Sara-Hasan <sara.ahabash@gmail.com>
2022-01-27 23:47:39 +0200

    update

--- travelix/src/App.js
+++ travelix/src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Home from "./components/Home";
+// import Blogs from "../components/Home";
+// import Contact from "./components/Home";
+// import NoPage from "./components/Home";
+import Navbar from "./components/layout/Navbar";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        
+          <Route path='/Home' element={<Home />} />
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
