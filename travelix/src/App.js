a75587a14d52241fbd5902f8aa1930328676ce05
IbrahimKHasan <ibrahim.kha.hasan@gmail.com>
2022-01-28 14:18:40 +0200

    edit

--- travelix/src/App.js
+++ travelix/src/App.js
@@ -1,47 +1,46 @@
-import {React , Component} from 'react'
+import { React, Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
-import './elements_responsive.css'
-import './elements_styles.css'
+import "./App.css";
+import "./elements_responsive.css";
+import "./elements_styles.css";
 import Home from "./components/Home";
 import Navbar from "./components/layout/Navbar";
-import Landing from './components/landing/Landing';
-import Category from './components/category/Category';
-
+import Landing from "./components/landing/Landing";
+import Category from "./components/category/Category";
+import User from "./components/user/User";
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      categories : []
-    }
+      categories: [],
+    };
   }
 
   async componentDidMount() {
-    const url     = "http://localhost/project-react1/travelix/category.php";
-    const res     = await fetch(url);
-    const data    = await res.json();
+    const url = "http://localhost/project-react1/travelix/category.php";
+    const res = await fetch(url);
+    const data = await res.json();
 
     this.setState({
-      categories : data
-    })
+      categories: data,
+    });
   }
 
   render() {
     return (
-
-    <BrowserRouter>
-      <div className="App">
-      <Navbar />
-      <Routes>
-          <Route path='/Home' element={<Home />} />
-          {/* <Route path="blogs" element={<Blogs />} />
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/User" element={<User />} />
+            {/* <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} /> */}
-      </Routes>  
-        <Landing categories={this.state.categories} />
-      </div>
+          </Routes>
+          {/* <Landing categories={this.state.categories} /> */}
+        </div>
       </BrowserRouter>
     );
   }
