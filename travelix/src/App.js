6d94bd75bf38f11b951ed3d274ffdbf02310151f
Ahmad-Kilani-Dev <40439458+dev-kilani@users.noreply.github.com>
2022-01-28 17:49:27 +0300

    added contact folder

--- travelix/src/App.js
+++ travelix/src/App.js
@@ -6,7 +6,7 @@ import './elements_styles.css'
 import Home from "./components/Home";
 import Navbar from "./components/layout/Navbar";
 import Landing from './components/landing/Landing';
-import Category from './components/category/Category';
+import Contact from './components/contact/Contact'
 
 
 class App extends Component {
@@ -35,10 +35,11 @@ class App extends Component {
       <div className="App">
       <Navbar />
       <Routes>
+         
           <Route path='/Home' element={<Home />} />
-          {/* <Route path="blogs" element={<Blogs />} />
+          {/* <Route path="blogs" element={<Blogs />} /> */}
           <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
       </Routes>  
         <Landing categories={this.state.categories} />
       </div>
---
+++ travelix/src/components/contact/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './contact_responsive.css';
+import './contact_styles.css';
+
+
+export class Contact extends Component {
+  render() {
+    return(
+    <div>
+
+    </div>
+    ) 
+  }
+}
+
+export default Contact;
