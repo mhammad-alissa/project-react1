855fccfb4ea6c67af3a2858c3d70dbb452a62cc6
Ahmad-Kilani-Dev <40439458+dev-kilani@users.noreply.github.com>
2022-01-29 11:16:59 +0300

    style fixes

--- travelix/src/components/contact/Contact.js
+++ travelix/src/components/contact/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './contact.css'
 // import { Link } from 'react-router-dom';
 
 export class Contact extends Component {
@@ -82,16 +83,16 @@ export class Contact extends Component {
 		</div>
 
 		{/* <!-- Contact --> */}
-		<div className="container mt-5 mb-5">
+		<div className="container C-container">
 			<div className="row">
-				<div className="col-lg-10" >
+				<div className="col-lg-12" >
 					<div className="contact_form_container">
 						<div className="contact_title">get in touch</div>
 						<form id="contact_form" className="contact_form">
-							<input type="text"  value={this.state.name} onChange={this.handleNameChange} id="contact_form_name" className="contact_form_name input_field" placeholder="Name" required="required" data-error="Name is required."/>
-							<input type="text"  value={this.state.email} onChange={this.handleEmailChange} id="contact_form_email" className="contact_form_email input_field" placeholder="E-mail" required="required" data-error="Email is required."/>
-							<input type="text"  value={this.state.subject} onChange={this.handleSubjectChange} id="contact_form_subject" className="contact_form_subject input_field" placeholder="Subject" required="required" data-error="Subject is required."/>
-							<textarea id="contact_form_message"  value={this.state.message} onChange={this.handleMessageChange} className="text_field contact_form_message" name="message" rows="4" placeholder="Message" required="required" data-error="Please, write us a message."></textarea>
+							<input type="text"  value={this.state.name} onChange={this.handleNameChange} id="contact_form_name" className="contact_form_name input_field" placeholder="Name" required data-error="Name is required."/>
+							<input type="text"  value={this.state.email} onChange={this.handleEmailChange} id="contact_form_email" className="contact_form_email input_field" placeholder="E-mail" required  data-error="Email is required."/>
+							<input type="text"  value={this.state.subject} onChange={this.handleSubjectChange} id="contact_form_subject" className="contact_form_subject input_field" placeholder="Subject" required  data-error="Subject is required."/>
+							<textarea id="contact_form_message"  value={this.state.message} onChange={this.handleMessageChange} className="text_field contact_form_message" name="message" rows="4" placeholder="Message" required  data-error="Please, write us a message."></textarea>
 							<button type="submit" value="submit" id="form_submit_button" onClick={this.handleSubmitChange} className="form_submit_button button">send message<span></span><span></span><span></span></button>
 						</form>
 					</div>
@@ -102,7 +103,7 @@ export class Contact extends Component {
 
 	{/* <!-- About --> */}
 	<div className="about">
-		<div className="container">
+		<div className="container aboutContainer">
 			<div className="row">
 				<div className="col-lg-5">
 					
