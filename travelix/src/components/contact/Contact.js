8006edf7969bfa1faf1f66a0cbf8c81218fa56a9
Ahmad-Kilani-Dev <40439458+dev-kilani@users.noreply.github.com>
2022-01-29 20:22:38 +0300

    fixing stuff

--- travelix/src/components/contact/Contact.js
+++ travelix/src/components/contact/Contact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+// import Swal from "sweetalert2";
 import './contact.css'
-// import { Link } from 'react-router-dom';
 
 export class Contact extends Component {
 
@@ -21,46 +21,66 @@ export class Contact extends Component {
 	}
 
 	handleNameChange=(event)=>{
+		
 		this.setState({
 			name:event.target.value
 		})
 	}
 	handleEmailChange=(event)=>{
+		
 		this.setState({
 			email:event.target.value
 		})
 	}
 	handleSubjectChange=(event)=>{
+	
 		this.setState({
 			subject:event.target.value
 		})
 	}
 	handleMessageChange=(event)=>{
+	
 		this.setState({
 			message:event.target.value
 		})
 	}
 	handleSubmitChange=(event)=>{
+		
 		event.preventDefault();
 		console.log(event);
-
+		
 		const obj ={
 			name:this.state.name,
         	email:this.state.email,
 			subject:this.state.subject,
-			message:this.state.message
+			message:this.state.message,
           };
 
 		axios.post('http://localhost/project-react1/php/contact.php',obj)
 
+
+// 		if(){
+// 			Swal.fire(
+// 			'Sent Succesfully',
+// 			'Thank you for contacting us',
+// 			'success'
+// 			)
+// }
 		
+
+		.catch(error => {
+            console.log(error.response)
+        });
 
 		this.setState({
 			name:"",
 			email:"",
 			subject:"",
-			message:""
+			message:"",
 		})
+
+	
+		
 		
 	}
 
@@ -88,11 +108,15 @@ export class Contact extends Component {
 				<div className="col-lg-12" >
 					<div className="contact_form_container">
 						<div className="contact_title">get in touch</div>
-						<form id="contact_form" className="contact_form">
-							<input type="text"  value={this.state.name} onChange={this.handleNameChange} id="contact_form_name" className="contact_form_name input_field" placeholder="Name" required data-error="Name is required."/>
+						<form id="contact_form" className="contact_form" noValidate>
+							<input type="text"  required value={this.state.name} onChange={this.handleNameChange} id="contact_form_name" className="contact_form_name input_field" placeholder="Name"  data-error="Name is required."/>
+							{/* <span>value={this.state.error}</span> */}
 							<input type="text"  value={this.state.email} onChange={this.handleEmailChange} id="contact_form_email" className="contact_form_email input_field" placeholder="E-mail" required  data-error="Email is required."/>
+							{/* <span>value={this.state.error}</span> */}
 							<input type="text"  value={this.state.subject} onChange={this.handleSubjectChange} id="contact_form_subject" className="contact_form_subject input_field" placeholder="Subject" required  data-error="Subject is required."/>
+							{/* <span>value={this.state.error}</span> */}
 							<textarea id="contact_form_message"  value={this.state.message} onChange={this.handleMessageChange} className="text_field contact_form_message" name="message" rows="4" placeholder="Message" required  data-error="Please, write us a message."></textarea>
+							{/* <span>value={this.state.error}</span> */}
 							<button type="submit" value="submit" id="form_submit_button" onClick={this.handleSubmitChange} className="form_submit_button button">send message<span></span><span></span><span></span></button>
 						</form>
 					</div>
@@ -115,7 +139,7 @@ export class Contact extends Component {
 
 				</div>
 
-				<div className="col-lg-4">
+				<div className="col-lg-4 About-text">
 					
 					{/* <!-- About - Content --> */}
 
@@ -123,7 +147,12 @@ export class Contact extends Component {
 						<div className="logo_container about_logo">
 							<div className="logo"><a href="#"><img src="images/logo.png" alt="" />travelix</a></div>
 						</div>
-						<p className="about_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis vu lputate eros, iaculis consequat nisl. Nunc et suscipit urna. Integer eleme ntum orci eu vehicula iaculis consequat nisl. Nunc et suscipit urna pretium.</p>
+						<p className="about_text">GOING BEYOND SATISFACTION
+								To deliver the best experience each day, we personalize each point of contact with great 
+								care and attention to detail, whether for our clients, partners or employees. We consistently
+								strive for excellence
+								and share the desire to
+								surpass expectations and offer the best to our members.</p>
 						<ul className="about_social_list">
 							<li className="about_social_item"><a href="#"><i className="fa fa-pinterest"></i></a></li>
 							<li className="about_social_item"><a href="#"><i className="fa fa-facebook-f"></i></a></li>
